5b11db1c6fb13d8e457c63b934963311336a0dbc
Y.Gagan <gagannnaidu2006@gmail.com>
2024-03-08 20:10:23 +0530

    updated the opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,29 @@ export const metadata: Metadata = {
     card:"summary_large_image",
     site:"@aanm_vvrsr_polytechnic",
     images:"https://aanm-vvrsr.vercel.app/og.png"
-  }
+  },
+  openGraph: {
+    title: 'AANM & VVRSR Polytechnic',
+    description: 'Best Polytechnic in Andhra Pradesh',
+    url: 'https://aanm-vvrsr.vercel.app/',
+    siteName: 'AANM & VVRSR Polytechnic',
+    images: [
+      {
+        url: 'https://aanm-vvrsr-3q01p6wfo-ygns-projects.vercel.app/opengraph-image.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://aanm-vvrsr-3q01p6wfo-ygns-projects.vercel.app/opengraph-image.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
   };
 
 export default function RootLayout({
