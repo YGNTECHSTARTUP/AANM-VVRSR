0a2708190573ba2290226410907345e206aa92f9
Y.Gagan <gagannnaidu2006@gmail.com>
2024-03-17 22:11:10 +0530

    linked to my project

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,4 +1,16 @@
 // metadata.ts
 export const metadata = {
     icon: '/favicon.ico',
+    metadataBase: new URL('https://aanm-vvrsr.vercel.app/'),
+    alternates: {
+      canonical: '/',
+      languages: {
+        'en-US': '/en-US',
+        'de-DE': '/de-DE',
+      },
+    },
+    openGraph: {
+      images: '/og-image.png',
+    },
   }
+  
